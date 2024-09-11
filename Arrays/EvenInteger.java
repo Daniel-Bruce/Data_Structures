ce29ea9e30a7248fd79efde76efaac9b098079cc
Bruce <brucedaniel724@gmail.com>
2024-09-11 12:17:44 +0300

    removing even numbers from an array

---
+++ Arrays/EvenInteger.java
@@ -0,0 +1,48 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class EvenIntegers {
+    static int[] arrays = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 14, 13, 22, 34, 37, 56, 47};
+
+    public static void main(String[] args) {
+        // Calling the removeEven method with the arrays as an argument
+        int[] result = removeEven(arrays);
+
+        // Printing the array without even numbers
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+
+    }
+
+    // Method to remove even numbers from an array
+    public static int[] removeEven(int[] arrays) {
+        int oddCount = 0;
+
+        // Counting the number of odd numbers
+        for (int i = 0; i < arrays.length; i++) {
+            if (arrays[i] % 2 != 0) {
+                oddCount++;
+            }
+        }
+
+        // Creating a new array to store only odd numbers
+        //size of result will be same as the number of odd Numbers found
+        int[] result = new int[oddCount];
+        int idx = 0;
+
+        // Adding odd numbers to the result array
+        for (int i = 0; i < arrays.length; i++) {
+            if (arrays[i] % 2 != 0) {
+                result[idx] = arrays[i];
+                idx++;
+            }
+        }
+
+        // Returning the array with only odd numbers
+        return result;
+    }
+}
+
+
---
+++ Arrays/EvenIntegers.java
@@ -0,0 +1,41 @@
+package Arrays;
+
+public class EvenIntegers {
+          static  int [] arrays = {1,2,3,4,5,6,7,8,9,10,14,13,22,34,37,56,47};
+
+          public static int[] removeEven(int[]arrays){
+            //initialize variable that will store the count of oddNUmbers in the array
+            int oddCount = 0;
+            //loop array so as to get count of oddNumbers
+            for (int i = 0; i < arrays.length; i++) {
+               if (arrays[i] % 2 != 0) {
+                  oddCount++;
+                  }
+                  }
+
+                   //This array will contain the odd numbers of size oddCount
+               int[] result = new int[oddCount];
+               int idx = 0;
+               // traverse to get the odd numbers
+               for (int i = 0; i < arrays.length; i++) {
+                  if (arrays[i] % 2 != 0) {
+                     result[idx] = arrays[i];
+                     idx++;
+                  }}
+                  return result;
+               }
+               public static void main(String[] args) {
+                  // int[] oddNumbers = removeEven(arrays);
+                  // for(int i: oddNumbers){
+                  //    System.out.print(i+ " ");
+                  // }}
+
+                  for (int num : removeEven(arrays)) {
+                     System.out.println(num);
+                  }
+
+               }
+                  }
+
+
+   
