7878ede9fab4bc5e20a16cb79160b2c28e16e61a
Bruce <brucedaniel724@gmail.com>
2024-09-11 12:24:55 +0300

    searching code example

---
+++ Search/BinarySearch.java
@@ -0,0 +1,52 @@
+package Data_Structures;
+
+
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int num[] = {4,5,6,7,8,9,11};
+        int target = 9;
+
+        int result = binarySearch(num,target,0,num.length-1);
+
+        if (result!= -1)
+            System.out.println("Element found at index : "+result);
+        else
+            System.out.println("Element not found");
+    }
+
+
+    public static int binarySearch(int[] num, int target,int left,int right) {
+//        int left = 0;
+//        int right = num.length - 1;
+//
+//        while(left <= right ) {
+//            int mid = (left + right) / 2;
+//
+//            if (num[mid] == target) {
+//              return mid;
+//            } else if (num[mid] < target) {
+//                left = mid + 1;
+//            }else{
+//                right = mid - 1;
+//            }
+//        }
+//        return -1;
+
+        if (left <= right ) {
+          int mid = (left+right)/2;
+            if (num[mid] == target) {
+              return mid;
+            } else if (num[mid] < target) {
+                return  binarySearch(num,target,mid+1,right);
+            }else{
+                return  binarySearch(num,target,left,mid-1);
+            }
+        }
+        return -1;
+        }
+    }
+
+
+    
+
---
+++ Search/LinearSearch.java
@@ -0,0 +1,25 @@
+package Data_Structures;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        int num[] = {4,5,6,7,8,9,11};
+        int target = 1;
+
+        int result = linearSearch(num,target);
+
+        if (result!= -1)
+           System.out.println("Element found at index : "+result);
+        else 
+           System.out.println("Element not found");
+    }
+
+
+    public static int linearSearch(int[] num, int target) {
+        for (int i = 0; i < num.length; i++) {
+            if (num[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
