794e66b73b88dbae3deb8c3b29a2924dba683af9
Bruce <brucedaniel724@gmail.com>
2024-09-12 17:15:26 +0300

    Find the minimum value in an array

---
+++ Arrays/MinimumArrayValue.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class MinimumArrayValue {
+   static int[] arr = {67,8,6,5,6,3,2,7,4,1};
+    public static int minumumValue(int[] arr){
+        int num = arr[0]; // will hold the minimum array value
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < num) {
+                num = arr[i];
+            }
+        }
+        return num;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(minumumValue(arr));
+    }
+}
