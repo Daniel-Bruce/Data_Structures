650cc1a0adf51b315468b788f7410115fb5b81e9
Bruce <brucedaniel724@gmail.com>
2024-09-12 17:19:34 +0300

    Looking for second maximum value in an array

---
+++ Arrays/SecondMaximumArrayValue.java
@@ -0,0 +1,26 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class SecondMaximumArrayValue {
+    static int[] arr = {1,2,4,5,6,76,75,77,34,65,78,98,3,24,5,7,86};
+    public static int secondMaximumNumber(int[]arr){
+        int max = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            int num = arr[i];
+            if (num > max) {
+                secondMax = max;
+                max = num;
+            } else if (num != max && num > secondMax) {
+                secondMax = num;
+            }
+        }
+        return secondMax;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(secondMaximumNumber(arr));
+    }
+}
