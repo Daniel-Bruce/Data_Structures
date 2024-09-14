93d79b399feea184b18b5fc7cec1ae3d3c5c4752
Bruce <brucedaniel724@gmail.com>
2024-09-14 13:47:01 +0300

    Moves zero elements to end of an array but uses xtra space due to creation of new array

---
+++ Arrays/RemoveZeros.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class RemoveZeros {
+    static int[] arr = {0,1,0,4,12};
+    public static int[] removeZero(int[]arr){
+        int[] result = new int[arr.length];
+        int idx = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0){
+                result[idx] = arr[i];
+                idx++;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(removeZero(arr)));
+    }
+}
