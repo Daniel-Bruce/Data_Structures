9a98ea3aff6a88898b4b3e324fdde0fd07b3c9a2
Bruce <brucedaniel724@gmail.com>
2024-09-18 23:01:02 +0300

    Move zeros to end of array

---
+++ Arrays/MoveZeros.java
@@ -0,0 +1,29 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class MoveZeros {
+    static int[] arr = {0, 1, 0, 4, 12};
+
+    public static void moveZeroes(int[] arr) {
+        int idx = 0; // Index to place the next non-zero element
+
+        // Move all non-zero elements to the front
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                arr[idx] = arr[i];
+                idx++;
+            }
+        }
+
+        while (idx < arr.length) {
+            arr[idx] = 0;
+            idx++;
+        }    }
+
+    public static void main(String[] args) {
+        moveZeroes(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
+
