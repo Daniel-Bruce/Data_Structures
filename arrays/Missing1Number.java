a30936e9b66b18c234e6a4c21f228fb9e51a6217
Bruce <brucedaniel724@gmail.com>
2024-10-08 21:33:14 +0300

    updated way of finding missing number in array

--- arrays/Missing1Number.java
+++ arrays/Missing1Number.java
@@ -1,29 +1,17 @@
 package arrays;
 
 public class Missing1Number {
-    static int[] arr = new int[99];
-    public static void main(String[] args) {
-        int missingNumber = 77;
-        int index = 0;
-        for (int i = 1; i <= 100; i++) {
-            if (i != missingNumber) {
-                arr[index++] = i;
-                }
-        }
-        int missingint = missingInt(arr, 100);
-        System.out.println("The missing number is:"+missingint);
-        
+   static int findMissing(int[]arr){
+    int n = arr.length + 1; //because we are missing one number in the array
+    int totalSum = (n * (n + 1)) / 2;
+    for (int num : arr) {
+        totalSum = totalSum - num;
     }
-    static int missingInt(int[]arr,int n){
-        int expectedSum = n*(n+1)/2;
-
-        int sum = 0;
-
-        for (int i : arr) {
-            sum+=i;
-        }
-
-        return expectedSum - sum;
+    return totalSum;
+   }
+   public static void main(String[] args) {
+    int[] arr = {1, 2, 4,3,5,6,8,9};
+    System.out.println(findMissing(arr));
     }
 }
 
